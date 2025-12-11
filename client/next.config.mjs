97bf11ca0ed17df11fb767333b1c05d2714015e2
eezlogicxarvind <159049532+voidloopxarvin@users.noreply.github.com>
2025-12-11 18:35:57 +0530

    Update next.config.mjs

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,6 +1,20 @@
+// // /** @type {import('next').NextConfig} */
+// // const nextConfig = {
+// //   output: 'standalone',
+// //   eslint: {
+// //     ignoreDuringBuilds: true,
+// //   },
+// //   typescript: {
+// //     ignoreBuildErrors: true,
+// //   },
+// //   images: {
+// //     unoptimized: true,
+// //   },
+// // };
+
+// // export default nextConfig;
 // /** @type {import('next').NextConfig} */
 // const nextConfig = {
-//   output: 'standalone',
 //   eslint: {
 //     ignoreDuringBuilds: true,
 //   },
@@ -23,6 +37,14 @@ const nextConfig = {
   },
   images: {
     unoptimized: true,
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://13.60.228.51/api/:path*',
+      },
+    ];
   },
 };
 
