3f66ae49f2bf6c188578c2ef05ee933b83027332
asdfqwert <159049532+wizif@users.noreply.github.com>
2026-07-23 10:44:29 +0530

    Add SocketContextType interface for socket state

--- client/src/contexts/SocketContext.tsx
+++ client/src/contexts/SocketContext.tsx
@@ -2,7 +2,7 @@
 
 import React, { createContext, useContext, useEffect, useState } from 'react';
 import { io, Socket } from 'socket.io-client';
-
+ 
 interface SocketContextType {
   socket: Socket | null;
   isConnected: boolean;
